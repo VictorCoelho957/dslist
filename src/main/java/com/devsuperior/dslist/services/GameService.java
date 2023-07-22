876eda7e174120ff78cc4ac7af517b80eecd519d
Perkles <victor.coelho@discente.ufma.br>
2023-07-21 22:03:28 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,22 +4,34 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+
 
 @Service  //registar como como componente do sistema
 public class GameService {
 	
 	@Autowired
+	
 	private GameRepository gameRepository; //injeção da interface GameRepository nesta classe
 	
+	@Transactional(readOnly=true)//
+	public GameDTO findById(Long id) {
+		Game result=gameRepository.findById(id).get(); //buscando no banco
+		return new GameDTO(result); //convertendo para DTO	
+	}
+	
+	@Transactional(readOnly=true)//
 	public List<GameMinDTO> findAll(){
 		List<Game> result=gameRepository.findAll();
 		List<GameMinDTO> dto= result.stream().map(x-> new GameMinDTO(x)).toList();
 		return dto;
 	}
+	
 
 }
